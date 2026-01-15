585e0777d8f0a9a7a0f4322e821d8cb70db404a0
harihardiyan <lorozloraz@gmail.com>
2026-01-16 05:39:46 +0700

    Create hamiltonians.py

---
+++ topo_dw/hamiltonians.py
@@ -0,0 +1,51 @@
+import jax
+jax.config.update("jax_enable_x64", True)
+import jax.numpy as jnp
+
+sx = jnp.array([[0, 1],
+                [1, 0]], dtype=jnp.complex128)
+sy = jnp.array([[0, -1j],
+                [1j,  0]], dtype=jnp.complex128)
+sz = jnp.array([[1,  0],
+                [0, -1]], dtype=jnp.complex128)
+
+gamma_x = sx
+gamma_y = sy
+gamma_0 = sz
+
+def build_H_2d_wilson_kx(kx, Ny, m_y, r=1.0):
+    dim = 2 * Ny
+    H = jnp.zeros((dim, dim), dtype=jnp.complex128)
+
+    sin_kx = jnp.sin(kx)
+    cos_kx = jnp.cos(kx)
+
+    W_x = r * (1.0 - cos_kx)
+
+    Hx_dirac = sin_kx * gamma_x
+    Hx_wilson = W_x * gamma_0
+
+    Hy_forward_dirac  = -0.5j * gamma_y
+    Hy_backward_dirac =  0.5j * gamma_y
+
+    Hy_forward_wilson  = -0.5 * r * gamma_0
+    Hy_backward_wilson = -0.5 * r * gamma_0
+    Hy_onsite_wilson   = r * gamma_0
+
+    for j in range(Ny):
+        i0 = 2 * j
+        i1 = i0 + 2
+        m_loc = m_y[j]
+        onsite = m_loc * gamma_0 + Hx_dirac + Hx_wilson + Hy_onsite_wilson
+        H = H.at[i0:i1, i0:i1].set(H[i0:i1, i0:i1] + onsite)
+
+    for j in range(Ny - 1):
+        i0 = 2 * j
+        i1 = i0 + 2
+        k0 = 2 * (j + 1)
+        k1 = k0 + 2
+
+        H = H.at[k0:k1, i0:i1].set(H[k0:k1, i0:i1] + Hy_forward_dirac + Hy_forward_wilson)
+        H = H.at[i0:i1, k0:k1].set(H[i0:i1, k0:k1] + Hy_backward_dirac + Hy_backward_wilson)
+
+    return H
