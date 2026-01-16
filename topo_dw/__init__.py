1503de78ca8d3561cc066c409548ab48f231238e
harihardiyan <lorozloraz@gmail.com>
2026-01-16 16:59:12 +0700

    Update __init__.py

--- topo_dw/__init__.py
+++ topo_dw/__init__.py
@@ -1,3 +1,5 @@
+# Public API exposure
+
 from .mass_profiles import mass_step_profile_y, mass_kink_profile_y
 from .hamiltonians import build_H_2d_wilson_kx
 from .spectrum import spectrum_2d_wilson, spectral_flow_2d_wilson
