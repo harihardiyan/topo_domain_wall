a001f7c0737a1eb7916aaf45a2f2a419556441a0
harihardiyan <lorozloraz@gmail.com>
2026-01-16 17:02:07 +0700

    Update spectrum.py

--- topo_dw/spectrum.py
+++ topo_dw/spectrum.py
@@ -22,10 +22,4 @@ def spectral_flow_2d_wilson(m_y, Ny, Nkx=101, n_modes=4, r=1.0):
 
     def low_evals_at_kx(kx):
         Hkx = build_H_2d_wilson_kx(kx, Ny, m_y, r=r)
-        evals, _ = jnp.linalg.eigh(Hkx)
-        idx_sorted = jnp.argsort(jnp.abs(evals))
-        sel = idx_sorted[:n_modes]
-        return evals[sel]
-
-    E_low = jax.vmap(low_evals_at_kx)(kxs)
-    return kxs, E_low
+        evals, _ = jnp.linalg
