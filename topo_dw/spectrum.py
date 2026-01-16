5500a8196d0946a7420213800d6e74210b171bf5
harihardiyan <lorozloraz@gmail.com>
2026-01-16 17:48:20 +0700

    Update spectrum.py

--- topo_dw/spectrum.py
+++ topo_dw/spectrum.py
@@ -2,10 +2,11 @@ import jax
 import jax.numpy as jnp
 from .hamiltonians import build_H_2d_wilson_kx
 
+
+# ADA 2 BARIS KOSONG DI ATAS
 def spectrum_2d_wilson(m_y, Ny, Nkx=201, r=1.0):
     kxs = jnp.linspace(-jnp.pi, jnp.pi, Nkx)
 
-    @jax.jit
     def evals_at_kx(kx):
         Hkx = build_H_2d_wilson_kx(kx, Ny, m_y, r=r)
         evals, _ = jnp.linalg.eigh(Hkx)
@@ -14,10 +15,11 @@ def spectrum_2d_wilson(m_y, Ny, Nkx=201, r=1.0):
     E = jax.vmap(evals_at_kx)(kxs)
     return kxs, E
 
+
+# ADA 2 BARIS KOSONG DI ATAS
 def spectral_flow_2d_wilson(m_y, Ny, Nkx=101, n_modes=4, r=1.0):
     kxs = jnp.linspace(-jnp.pi, jnp.pi, Nkx)
 
-    @jax.jit
     def low_evals_at_kx(kx):
         Hkx = build_H_2d_wilson_kx(kx, Ny, m_y, r=r)
         evals, _ = jnp.linalg.eigh(Hkx)
