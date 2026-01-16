d3902faa747be3eee762302fdddc8c853ef6afcf
harihardiyan <lorozloraz@gmail.com>
2026-01-16 17:42:32 +0700

    Update plotting.py

--- topo_dw/plotting.py
+++ topo_dw/plotting.py
@@ -1,9 +1,12 @@
 import matplotlib
-matplotlib.use("Agg")
 import matplotlib.pyplot as plt
 
+
+matplotlib.use("Agg")
+
+
 def plot_spectrum(kx, E, title, filename):
-    plt.figure(figsize=(6,4))
+    plt.figure(figsize=(6, 4))
     for n in range(E.shape[1]):
         plt.plot(kx, E[:, n], 'k-', lw=0.4)
     plt.axhline(0, color='r', lw=0.8)
@@ -14,8 +17,9 @@ def plot_spectrum(kx, E, title, filename):
     plt.savefig(filename, dpi=150)
     plt.close()
 
+
 def plot_interface_modes(ys, Dens, Es, title, filename):
-    plt.figure(figsize=(6,4))
+    plt.figure(figsize=(6, 4))
     for i in range(Dens.shape[0]):
         plt.plot(ys, Dens[i], label=f"mode {i}, E={Es[i]:.3f}")
     plt.title(title)
@@ -26,8 +30,9 @@ def plot_interface_modes(ys, Dens, Es, title, filename):
     plt.savefig(filename, dpi=150)
     plt.close()
 
+
 def plot_spectral_flow(kx, E_flow, title, filename):
-    plt.figure(figsize=(6,4))
+    plt.figure(figsize=(6, 4))
     for i in range(E_flow.shape[1]):
         plt.plot(kx, E_flow[:, i], lw=1.2)
     plt.title(title)
