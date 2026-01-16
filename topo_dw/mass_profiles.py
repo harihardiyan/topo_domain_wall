ad687a82a34ea95b3237e5a4aa11ccf1bdac3d39
harihardiyan <lorozloraz@gmail.com>
2026-01-16 17:55:22 +0700

    Update mass_profiles.py

--- topo_dw/mass_profiles.py
+++ topo_dw/mass_profiles.py
@@ -1,8 +1,6 @@
 import jax.numpy as jnp
 
 
-# BLANK LINE 1
-# BLANK LINE 2
 def mass_step_profile_y(Ny, m_top, m_bottom):
     m_y = []
     for j in range(Ny):
@@ -13,8 +11,6 @@ def mass_step_profile_y(Ny, m_top, m_bottom):
     return jnp.array(m_y, dtype=jnp.float64)
 
 
-# BLANK LINE 1
-# BLANK LINE 2
 def mass_kink_profile_y(Ny, m0=1.0, width=5.0):
     ys = jnp.arange(Ny)
     y0 = (Ny - 1) / 2.0
