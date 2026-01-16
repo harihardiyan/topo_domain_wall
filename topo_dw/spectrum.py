1927efea653083e1aab65d128fb4121f2fc5e780
harihardiyan <lorozloraz@gmail.com>
2026-01-16 17:35:57 +0700

    Update spectrum.py

--- topo_dw/spectrum.py
+++ topo_dw/spectrum.py
@@ -1,22 +1,23 @@
 import jax
-jax.config.update("jax_enable_x64", True)
 import jax.numpy as jnp
-
 from .hamiltonians import build_H_2d_wilson_kx
 
-
 def spectrum_2d_wilson(m_y, Ny, Nkx=201, r=1.0):
+    kxs = jnp.linspace(-jnp.pi, jnp.pi, Nkx)
+
+    @jax.jit
     def evals_at_kx(kx):
         Hkx = build_H_2d_wilson_kx(kx, Ny, m_y, r=r)
         evals, _ = jnp.linalg.eigh(Hkx)
         return evals
 
-    kxs = jnp.linspace(-jnp.pi, jnp.pi, Nkx)
     E = jax.vmap(evals_at_kx)(kxs)
     return kxs, E
 
-
 def spectral_flow_2d_wilson(m_y, Ny, Nkx=101, n_modes=4, r=1.0):
+    kxs = jnp.linspace(-jnp.pi, jnp.pi, Nkx)
+
+    @jax.jit
     def low_evals_at_kx(kx):
         Hkx = build_H_2d_wilson_kx(kx, Ny, m_y, r=r)
         evals, _ = jnp.linalg.eigh(Hkx)
@@ -24,6 +25,5 @@ def spectral_flow_2d_wilson(m_y, Ny, Nkx=101, n_modes=4, r=1.0):
         sel = idx_sorted[:n_modes]
         return evals[sel]
 
-    kxs = jnp.linspace(-jnp.pi, jnp.pi, Nkx)
     E_low = jax.vmap(low_evals_at_kx)(kxs)
     return kxs, E_low
